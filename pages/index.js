9b037f7a58e34f0ea2a5736a33e2c104558116f1
Elliot Hershberg <eahershberg@gmail.com>
2021-03-18 15:59:49 -0700

    Set ssr to false for dynamic import

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic'
 
-const Browser = dynamic(() => import('../components/Browser'))
+const Browser = dynamic(() => import('../components/Browser'), {ssr: false})
 
 export default function Home() {    
     return (
